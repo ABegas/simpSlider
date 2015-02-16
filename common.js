7620f20497493cb3b885422ee82475a5ac0886de
unknown <a.begas@abegas-nb.kiev.ecofabric.com>
2015-02-16 10:32:02 +0100

    Simp-1: files created

---
+++ common.js
@@ -0,0 +1,8 @@
+//тенарный оператор условия
+
+var age = 24;
+
+(age === 18) ? console.log('Вы взрослый') : 
+               (age === 21) ? console.log('вам 21'):
+               (age === 24) ? console.log('вам 24'):
+                    console.log('вам нет 18')
